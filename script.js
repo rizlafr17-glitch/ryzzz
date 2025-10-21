6a24fa79ddf6b54c28a2664139d8a3123b91611e
rizlafr17-glitch <rizlafr17@gmail.com>
2025-10-20 23:39:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
+
+const tombolMode = document.getElementById('toggleMode');
+
+tombolMode.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+});
